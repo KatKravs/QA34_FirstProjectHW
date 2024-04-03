bbb790b98665a661012942b2537e62be0bcb8707
Aliyev-Siyab <149569244+Aliyev-Siyab@users.noreply.github.com>
2024-04-03 23:14:06 +0200

    HW3findTenDiffferentElementsByXpath

--- src/test/java/com/ait/qa34/HWSeleniumTest.java
+++ src/test/java/com/ait/qa34/HWSeleniumTest.java
@@ -4,15 +4,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
-import java.util.HashSet;
+
 import java.util.List;
-import java.util.Set;
+
 
 public class HWSeleniumTest {
 
